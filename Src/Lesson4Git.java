ce07f91e3de1a1998d4bb81dc8861b36d964c08e
MAXZIMOVISHE <olexin.max@gmail.com>
2023-06-02 21:04:59 +0300

    message

--- Src/Lesson4Git.java
+++ Src/Lesson4Git.java
@@ -1,5 +1,6 @@
-public class Lesson4Git}
+public class Lesson4Git {
 
-public static void main (String [] args)}
-System.out.println ("Hello git"){
+public static void main (String [] args) {
+    System.out.println("Hello git");
+}
 }
